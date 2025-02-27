f3167940f783e843d7f24607cc88f24b558c9f8f
Osvaldo Garay <osvaldo.garay@youorder.me>
2025-02-27 14:32:38 -0300

    small fix

--- src/tools.py
+++ src/tools.py
@@ -2,6 +2,8 @@ import re
 from bs4 import BeautifulSoup
 
 def clean_text(text):
+    if not text:  # Ensure text is not None or empty
+        return ""
     soup = BeautifulSoup(text, "html.parser")
     cleaned_text = soup.get_text(" ")
     cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip()
