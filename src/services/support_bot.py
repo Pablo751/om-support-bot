433381c9ddc0eb205af391249baa58c2530611af
Osvaldo Garay <osvaldo.garay@youorder.me>
2025-03-03 15:01:55 -0300

    cleaned support bot message code

--- src/services/support_bot.py
+++ src/services/support_bot.py
@@ -18,21 +18,20 @@ class SupportBot:
         if message.id in self.processed_messages:
             raise Exception("Message already processed")
         self.processed_messages.append(message.id)
-        self.message = message
-        logger.info(f"Sending request to OpenAI query: {self.message.query}")
+        logger.info(f"Sending request to OpenAI query: {message.query}")
         query_response = self.openai.analyze_query(
             self.system_instructions, 
             self.knowledge_base.format(knowledge=self.knowledge_service.load_and_build_knowledge()),
-            self.message.query
+            message.query
         )
         logger.info(f"Generated query response: {query_response}")
 
-        act_response = self.act(query_response)
+        act_response = self.act(message, query_response)
         logger.info(f"Generated action response: {act_response}")
 
         return act_response
 
-    def act(self, query_response):
+    def act(self, message, query_response):
         process_again = query_response.get("process_again", False)
         query_type = query_response.get("query_type")
         act_response = query_response.get('response_text')
@@ -51,17 +50,17 @@ class SupportBot:
                 else:
                     act_response = texts.STORE_STATUS_INACTIVE_MSG.format(commerce_id=commerce_id, client_name=client_name)
         elif query_type == 'ESCALATE':
-            if self.message.type == 'whatsapp':
-                ticket_title = f"Escalation request for {self.message.type} (Phone Number: {self.message.id})"
-                api_response = self.message.create_ticket(subject=ticket_title, description=self.message.query)
+            if message.type == 'whatsapp':
+                ticket_title = f"Escalation request for {message.type} (Phone Number: {message.id})"
+                api_response = message.create_ticket(subject=ticket_title, description=message.query)
                 logger.info(f"Ticket create response: {api_response}")
             act_response = texts.ESCALATE_MSG
             
         if process_again:
-            self.message.query += act_response
-            return self.process_query(self.message)
+            message.query += act_response
+            return self.process_query(message)
         
-        send_response = self.message.reply(act_response)
+        send_response = message.reply(act_response)
         logger.info(f"Send response: {send_response}")
 
         return act_response
